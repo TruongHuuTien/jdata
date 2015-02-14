ebcd4663bcb1f69dccf3eccee98d325a50d67284
TRUONG Huu-Tien <Tien@Tien-MBP.local>
2015-02-14 09:49:19 -0500

    Add functions .html() .val() and .render()
    Add .html and .val wrapper function and .render()
    Add sample in demo’s file

--- jquery/jdata.js
+++ jquery/jdata.js
@@ -204,8 +204,28 @@ var jData = (function(){
 
 var jData = (function(JData, $){
 	
-	jData.appendTo = function($el, template, data) {
+	JData.append = function($el, template, data) {
 		$($el).append(this.apply(template, data).toString());
+	}
+	
+	JData.html = function($el, template, data) {
+		$($el).html(this.apply(template, data).toString());
+	}
+	
+	JData.val = function($el, template, data) {
+		$($el).val(this.apply(template, data).toString());
+	}
+	
+	JData.render = function($el, template, data) {
+		$($el).find('[jdata]').each(function() {
+			var $this = $(this);
+			var t = template[$this.attr('jdata')];
+			if ($this.is('input')) {
+				JData.val($this, t, data);
+			} else {
+				JData.html($this, t, data)
+			}
+		});
 	}
 	
 	return JData;
