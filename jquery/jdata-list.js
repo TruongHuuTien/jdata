25f2e02fb1f661e3d64490ddfcb0d40e6ea52ac9
TRUONG Huu-Tien <Tien@Tien-MBP.local>
2015-03-06 10:50:25 -0500

    jdatalist

--- jquery/jdata-list.js
+++ jquery/jdata-list.js
@@ -6,7 +6,26 @@
 /********************************************************************************************/
 
 (function($, jdata) {
-	$.fn.jdataList = function(template, data) {
-		console.log(this);
+	
+	var datalist = {};
+	
+	datalist.create = function($el, data) {
+		for (var i=0; i<data.length; i++) {
+			$el.append(this.createElement(data[i]));
+		}
 	}
+	
+	datalist.createElement = function(data) {
+		var $li = $('<li></li>');
+		
+		$li.attr('value', data.value);
+		$li.text(data.label);
+			
+		return $li;
+	}
+	
+	$.fn.jdataList = function(data) {
+		datalist.create(this, data);
+	}
+
 })($, jdata);
